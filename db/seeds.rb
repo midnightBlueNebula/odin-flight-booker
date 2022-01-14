ed8b5e181d496bb04e34497d92606e1e7e591eb1
Onur Kaya <46904292+midnightBlueNebula@users.noreply.github.com>
2022-01-14 19:07:54 +0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,22 @@ Airport.create(code: "LV", city: "Las Venturas")
 Airport.create(code: "LC", city: "Liberty City")
 Airport.create(code: "VC", city: "Vice City")
 Airport.create(code: "NSA", city: "Nos Astra")
+Airport.create(code: "ILL", city: "Illium")
+Airport.create(code: "OMG", city: "Omega")
+Airport.create(code: "SOL", city: "Solitude")
+Airport.create(code: "WHI", city: "Whiterun")
+Airport.create(code: "NOV", city: "Novigrad")
+Airport.create(code: "TOU", city: "Toussaint")
+Airport.create(code: "VEL", city: "Veluca")
+Airport.create(code: "SHA", city: "Shariz")
 
-10000.times do |n|
-    from = rand(9)
+
+
+1000000.times do |n|
+    from = rand(17)
     to = from 
     while to == from 
-        to = rand(9)
+        to = rand(17)
     end 
     Flight.create(take_off_time: Time.new + n*15.minutes, arrival_time: Time.new + n*15.minutes + 2.hours, 
         from_airport_id: from, to_airport_id: to)
